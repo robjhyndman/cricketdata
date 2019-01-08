16ef46dd4602f6a76583fbdb210a613a2b5c8909
Sayani Gupta <Sayani.Gupta@monash.edu>
2019-01-08 16:52:48 +1100

    - Function couldn't fetch female player's data as API changed. Fixed that. Now the program will search for match classes for males and females and get the url for the one which has records
    - Replaced suppressMEssages by SuppressWarnings in the Runs column. In the Runs column , we have numerics or DNB for which warning messages are introduced due to coercion

--- R/fetch_player.R
+++ R/fetch_player.R
@@ -35,10 +35,10 @@ fetch_player_data <- function(playerid,
                               activity =c("batting", "bowling", "fielding")) {
   matchtype <- tolower(matchtype)
   matchtype <- match.arg(matchtype)
-
+  
   activity <- tolower(activity)
   activity <- match.arg(activity)
-
+  
   # First figure out if player is female or male
   profile <- paste(
     "http://www.espncricinfo.com/ci/content/player/",
@@ -49,19 +49,32 @@ fetch_player_data <- function(playerid,
     stop("Player not found")
   }
   female <- length(grep("format=women", as.character(raw))) > 0
-
-  matchclass <- match(matchtype, c("test", "odi", "t20")) + (female * 7)
-
+  
+  matchclass_male <- match(matchtype, c("test", "odi", "t20"))
+  matchclass_female <- match(matchtype, c("test", "odi", "t20")) + 7
+  
   url <- paste(
     "http://stats.espncricinfo.com/ci/engine/player/",
     playerid,
     ".html?class=",
-    matchclass,
+    matchclass_male,
     ";template=results;type=", activity, ";view=innings;wrappertype=print",
     sep = ""
   )
-
-
+  raw <- try(xml2::read_html(url), silent = TRUE)
+  check1 <- rvest::html_table(raw)
+  
+  if ("No records available to match this query" %in% unlist(check1)) {
+    url <- paste(
+      "http://stats.espncricinfo.com/ci/engine/player/",
+      playerid,
+      ".html?class=",
+      matchclass_female,
+      ";template=results;type=", activity, ";view=innings;wrappertype=print",
+      sep = ""
+    )
+  }
+  
   raw <- try(xml2::read_html(url), silent = TRUE)
   if ("try-error" %in% class(raw)) {
     stop("Problem with URL")
@@ -72,36 +85,36 @@ fetch_player_data <- function(playerid,
   tab_all_rec <- rvest::html_table(raw)
   tab <- tab_all_rec[[4]]
   tab_no_rec <- tab_all_rec[[3]]
-
+  
   if ("No records available to match this query" %in% unlist(tab_no_rec)) {
     stop(paste("Player has never played", matchtype, "format", sep = " "), call. = F)
   }
   # Remove redundant missings columns
-
+  
   tab <- tibble::as_tibble(tab[, colSums(is.na(tab)) != NROW(tab)])
-
+  
   # Convert "-" to NA
   tab[tab == "-"] <- NA
-
+  
   # Make tidy column names columns
   tidy.col <- make.names(colnames(tab), unique = TRUE)
   colnames(tab) <- gsub(".", "_", tidy.col, fixed = TRUE)
   tidy.col <- colnames(tab)
-
+  
   # Convert some columns to numeric or Date
-
+  
   tab$Innings <- as.integer(tab$Inns)
   tab$Start_Date <- lubridate::dmy(tab$Start_Date)
   tab$Opposition <- substring(tab$Opposition, 3)
   tab$Ground <- as.character(tab$Ground)
-
+  
   ## order the elements, no difference for different activities
-
+  
   com_col <- c("Start_Date", "Innings", "Opposition", "Ground")
-
+  
   ##Removing "*" in the column `Runs` and converting it to numeric
-  tab$Runs <- suppressMessages(as.numeric(gsub("*", "", x = tab$Runs, fixed = TRUE)))
-
+  tab$Runs <- suppressWarnings(as.numeric(gsub("*", "", x = tab$Runs, fixed = TRUE)))
+  
   # Reorder columns
   return(
     tab[, c(com_col, tidy.col[!tidy.col %in% com_col])]
