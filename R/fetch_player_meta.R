301bcff035814ae120eea3b3eb5e14b437e527a3
Rob J Hyndman <robjhyndman@gmail.com>
2025-03-24 18:03:15 +1100

    Updated functions due to new cricinfo restrictions

--- R/fetch_player_meta.R
+++ R/fetch_player_meta.R
@@ -34,87 +34,55 @@ fetch_player_meta <- function(playerid) {
 }
 
 fetch_player_meta_individual <- function(playerid) {
-  # Read URL with player meta data
-  url <- paste0("https://www.espncricinfo.com/*/content/player/", playerid, ".html")
-  raw <- try(rvest::read_html(url), silent = TRUE)
-  if ("try-error" %in% class(raw)) {
+  # Set up empty output
+  output <- data.frame(
+    cricinfo_id = playerid,
+    name = NA_character_,
+    full_name = NA_character_,
+    country = NA_character_,
+    dob = as.Date(NA),
+    batting_style = NA_character_,
+    bowling_style = NA_character_
+  )
+  # Read JSON file with player meta data
+  url <- paste0("http://core.espnuk.org/v2/sports/cricket/athletes/", playerid)
+  json <- try(jsonlite::read_json(url), silent = TRUE)
+  if ("try-error" %in% class(json)) {
     warning(paste(
       "Cannot read player information from ESPNCricinfo for ID",
       playerid
     ))
-    # Empty data frame with one row
-    output <- data.frame(
-      cricinfo_id = playerid,
-      full_name = NA_character_,
-      country = NA_character_
-    )
   } else {
-    # Parse html from ESPNCricinfo
-    html <- rvest::read_html(url)
-    player.col <- html |>
-      rvest::html_elements(".ds-grid p") |>
-      rvest::html_text(trim = TRUE) |>
-      stringr::str_squish() |> 
-      matrix(nrow=2) 
-    colnames(player.col) <- player.col[1,]
-    player.col <- player.col[-1,,drop=FALSE] |> 
-      as.data.frame()
-    keep_cols <- which(colnames(player.col) %in%
-      c("Full Name", "Born", "Age", "Batting Style", "Bowling Style", "Playing Role"))
-    player.col <- player.col[,keep_cols,drop=FALSE]
-    p.country.raw <- html |>
-      rvest::html_nodes(".ds-text-comfortable-s") |>
-      rvest::html_text(trim = TRUE)
-
-    # data frame with one row
-    output <- player.col |>
-      janitor::clean_names()
-    output$cricinfo_id <- playerid
-    output$country <- p.country.raw[1]
-  }
-  # Extract DOB and Birthplace
-  output$dob <- as.Date(NA)
-  output$birthplace <- NA_character_
-  if ("born" %in% colnames(output)) {
-    output$dob <- stringr::str_extract(output$born, "[A-Za-z0-9 ,]*[1-2][0-9][0-9][0-9]")
-    if (!is.na(output$dob)) {
-      # Is there a date or only a month and year?
-      if (stringr::str_detect(output$dob, ",")) {
-        output$dob <- lubridate::mdy(output$dob)
-      } else {
-        # Set date to first of month
-        output$dob <- lubridate::dmy(paste("01", output$dob))
-      }
-    }
-    output$birthplace <- stringr::str_remove(output$born, "[A-Za-z0-9 ,]*[0-9]")
-    output$born <- NULL
-    if (output$birthplace == "") {
-      output$birthplace <- NA_character_
-    } else {
-      output$birthplace <- stringr::str_remove(output$birthplace, "^[, ]*")
-    }
-    # Fix missing countries
+    output$full_name <- json$fullName 
+    output$name <- json$name
+    output$country <- get_espn_country(json$country)
     if(is.na(output$country)) {
-      output$country <- "South Korea"
-    } else if(stringr::str_detect(output$country, "INTL CAREER")) {
-      output$country <- "South Korea"
+      stop(paste("Country not found. Player",playerid,output$full_name))
+    }
+    output$dob <- as.Date(json$dateOfBirth)
+    if(length(json$style) > 0) {
+      output$batting_style <- json$style[[1]]$description
+    }
+    if(length(json$style) > 1) {
+      output$bowling_style <- json$style[[2]]$description
     }
   }
-  if (!("batting_style" %in% colnames(output))) {
-    output$batting_style <- NA_character_
-  }
-  if (!("bowling_style" %in% colnames(output))) {
-    output$bowling_style <- NA_character_
-  }
-  if (!("playing_role" %in% colnames(output))) {
-    output$playing_role <- NA_character_
-  }
+  return(output)
+}
 
-  tibble::as_tibble(output) |>
-    dplyr::select(
-      cricinfo_id, full_name, country, dob, birthplace, batting_style,
-      bowling_style, playing_role
-    )
+# Find country from ESPN team id
+get_espn_country <- function(i) {
+  if(i %in% espn_countries$id) {
+    return(espn_countries$country[espn_countries$id == i])
+  }
+  json <- try(jsonlite::read_json(paste0("http://core.espnuk.org/v2/sports/cricket/teams/", i)), silent = TRUE)
+  if(!("try-error" %in% class(json))) {
+    # Update data set to avoid repeated calls
+    espn_countries <- rbind(espn_countries, data.frame(id = i, country = json$name)) 
+    return(json$name)
+  } else {
+    return(NA_character_)
+  }
 }
 
 utils::globalVariables(c(
