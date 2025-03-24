d253478b5b98686fc55b741a12c12abe46e603c3
Rob J Hyndman <robjhyndman@gmail.com>
2025-03-25 09:25:53 +1100

    Don't export %>%

--- R/cricketdata.R
+++ R/cricketdata.R
@@ -1,13 +1,9 @@
-#' @importFrom dplyr %>%
 #' @importFrom dplyr pull
 #' @importFrom readr read_csv
 #' @importFrom tidyr separate pivot_wider
 #' @importFrom utils download.file read.csv unzip
 #' @importFrom tibble tibble as_tibble
 #'
-
-#' @export
-dplyr::`%>%`
 
 #' @keywords internal
 "_PACKAGE"
